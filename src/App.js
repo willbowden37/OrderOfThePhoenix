b9e70d4bde9b885edb3c9b11b7fce9b39dfe4827
joeldevey12 <deveyjoel@gmail.com>
2019-03-22 16:45:09 -0600

    Fixed path in index.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to Connect Four!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
