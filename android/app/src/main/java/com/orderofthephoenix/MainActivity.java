1992536e07c2c3ff7766d4d370bca38f34bc730a
willbowden37 <william.bowden@aggiemail.usu.edu>
2019-03-22 14:11:34 -0600

    Edited file for React Navigation

--- android/app/src/main/java/com/orderofthephoenix/MainActivity.java
+++ android/app/src/main/java/com/orderofthephoenix/MainActivity.java
@@ -1,6 +1,9 @@
 package com.orderofthephoenix;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "OrderOfThePhoenix";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
