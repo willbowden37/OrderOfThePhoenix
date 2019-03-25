567f4ed52e082d3c87e7e35815b88e002fe1b985
holmanjr <holmanjr17@gmail.com>
2019-03-25 14:04:17 -0600

    Added game board component

---
+++ src/components/GameBoard.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    TouchableOpacity
+} from 'react-native';
+import Button from './Button.js';
+
+export default class Button extends Component {
+    render() {
+        return (
+            <TouchableOpacity>
+                <Text>
+                    {this.createButtonGrid()}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+
+    createButtonGrid() {
+        let buttonGrid = []
+        for (let j = 0; j < 7; j++) {
+            let row = [];
+            for (let i = 0; i < 6; i++) {
+                let val = j * 5 + i;
+                row.push(<Button
+                    key={val}
+                    style={[{ flex: 1 }]}
+                    color='white' />)
+
+            }
+            buttonGrid.push(<View key={j}>{row}</View>)
+        }
+
+        return (<View>{buttonGrid}</View>);
+    }
+}
