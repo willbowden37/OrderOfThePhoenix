e8d710d908989ed41fc8f76bb8c46a6a37276a4a
holmanjr <holmanjr17@gmail.com>
2019-03-27 14:00:10 -0600

    created Tile component

---
+++ src/components/Tile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
+export default class Tile extends Component {
+	constructor(props) {
+		super(props);
+	}
+    render() {
+        return (
+			<TouchableOpacity
+			onPress={this.props.makeMove(this.props.key % 7)}
+			style = {{backgroundColor: this.props.color, padding: 10, margins: 10, borderWidth: 5, borderColor: black}}>
+                <Text>
+                    {this.props.name}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+}
