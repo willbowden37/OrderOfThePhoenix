be6a532cfcd2d06767218b98033bfd3e02b85321
holmanjr <holmanjr17@gmail.com>
2019-03-29 13:59:15 -0600

    change Text import to View

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
 import GameBoard from '../components/GameBoard';
 import style from '../Styles/style';
 
